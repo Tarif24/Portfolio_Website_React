ecf5558a63bb0ea2261ec2cdce40e041b9e89894
Tarif24 <tarif24@hotmail.com>
2025-03-28 01:03:27 -0400

    added styling to the project about pop up

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,15 +13,19 @@ const ProjectCard = ({ projectImg, name, githubLink, liveLink }) => {
         window.open(liveLink);
     };
 
-    const aboutStyle = `absolute top-0 left-0 right-0 flex flex-col justify-center items-center w-full h-full bg-gray-100 rounded-4xl border-[#555555] lg:w-full ${isAboutActive ? "block" : "hidden"}`;
+    const buttonStyle = `sm:text-[1rem] text-[0.9rem] font-medium border-2 rounded-[4rem] border-[#555555] align-text-center p-4 sm:w-[8rem] flex-1 transition duration-300 ease-in-out hover:text-white hover:bg-[#555555] hover:cursor-pointer ${isAboutActive ? "" : "z-1"}`;
+
+    const infoIconStyle = isAboutActive ? "text-white" : "text-black";
+
+    const aboutStyle = `absolute top-0 justify-self-center flex flex-col gap-4 justify-center items-center text-white w-full h-full bg-[#414141] border-8 rounded-4xl border-white transition duration-250 ease-in-out ${isAboutActive ? "opacity-100" : "opacity-0"}`;
 
     return (
         <div className="relative flex flex-1 flex-col gap-6 items-center p-6 bg-[#f7f7f7] border-2 rounded-4xl border-[#555555] lg:w-full md:w-[80%] justify-self-center">
             <div className={aboutStyle}>
-                <h1>About</h1>
-                <h1>Built With: HTML, CSS, JS</h1>
-                <h1>Start Date: 2025-03-28</h1>
-                <h1>End Date: 2025-03-28</h1>
+                <h1 className="text-[3rem] font-bold mb-8">About</h1>
+                <h1 className="text-[1.5rem]">Built With: HTML, CSS, JS</h1>
+                <h1 className="text-[1.5rem]">Start Date: 2025-03-28</h1>
+                <h1 className="text-[1.5rem]">End Date: 2025-03-28</h1>
             </div>
 
             <img
@@ -31,16 +35,10 @@ const ProjectCard = ({ projectImg, name, githubLink, liveLink }) => {
             />
             <h1 className="font-bold sm:text-3xl text-1xl">{name}</h1>
             <div className="flex gap-4 sm:w-fit w-full">
-                <button
-                    className="sm:text-[1rem] text-[0.9rem] font-medium border-2 rounded-[4rem] border-[#555555] align-text-center p-3 sm:w-[8rem] flex-1 transition duration-300 ease-in-out hover:text-white hover:bg-[#555555] hover:cursor-pointer"
-                    onClick={onGitHubClicked}
-                >
+                <button className={buttonStyle} onClick={onGitHubClicked}>
                     GitHub
                 </button>
-                <button
-                    className="sm:text-[1rem] text-[0.9rem] font-medium border-2 rounded-[4rem] border-[#555555] align-text-center p-4 sm:w-[8rem] flex-1 transition duration-300 ease-in-out hover:text-white hover:bg-[#555555] hover:cursor-pointer"
-                    onClick={onLiveClicked}
-                >
+                <button className={buttonStyle} onClick={onLiveClicked}>
                     Live Demo
                 </button>
             </div>
@@ -52,7 +50,10 @@ const ProjectCard = ({ projectImg, name, githubLink, liveLink }) => {
                         : setIsAboutActive(true);
                 }}
             >
-                <IoInformationCircleOutline size="2rem" />
+                <IoInformationCircleOutline
+                    size="2rem"
+                    className={infoIconStyle}
+                />
             </div>
         </div>
     );
