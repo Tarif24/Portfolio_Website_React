8152abb197663218fa4a5579c77a93df63c56d3f
Tarif24 <tarif24@hotmail.com>
2025-03-28 00:21:22 -0400

    Added a about section toggle for each project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { IoInformationCircleOutline } from "react-icons/io5";
 
 const ProjectCard = ({ projectImg, name, githubLink, liveLink }) => {
+    const [isAboutActive, setIsAboutActive] = useState(false);
+
     const onGitHubClicked = () => {
         window.open(githubLink);
     };
@@ -9,8 +13,17 @@ const ProjectCard = ({ projectImg, name, githubLink, liveLink }) => {
         window.open(liveLink);
     };
 
+    const aboutStyle = `absolute top-0 left-0 right-0 flex flex-col justify-center items-center w-full h-full bg-gray-100 rounded-4xl border-[#555555] lg:w-full ${isAboutActive ? "block" : "hidden"}`;
+
     return (
-        <div className="flex flex-1 flex-col gap-6 items-center p-6 bg-[#f7f7f7] border-2 rounded-4xl border-[#555555] lg:w-full md:w-[80%] justify-self-center">
+        <div className="relative flex flex-1 flex-col gap-6 items-center p-6 bg-[#f7f7f7] border-2 rounded-4xl border-[#555555] lg:w-full md:w-[80%] justify-self-center">
+            <div className={aboutStyle}>
+                <h1>About</h1>
+                <h1>Built With: HTML, CSS, JS</h1>
+                <h1>Start Date: 2025-03-28</h1>
+                <h1>End Date: 2025-03-28</h1>
+            </div>
+
             <img
                 className="sm:size-[90%] size-[100%] rounded-4xl border-1 border-[#555555]"
                 src={projectImg}
@@ -30,6 +43,16 @@ const ProjectCard = ({ projectImg, name, githubLink, liveLink }) => {
                 >
                     Live Demo
                 </button>
+            </div>
+            <div
+                className="absolute right-5 bottom-5 hover:cursor-pointer"
+                onClick={() => {
+                    isAboutActive
+                        ? setIsAboutActive(false)
+                        : setIsAboutActive(true);
+                }}
+            >
+                <IoInformationCircleOutline size="2rem" />
             </div>
         </div>
     );
